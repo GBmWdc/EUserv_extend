ce00a6015f2d020223155f08a54e6df913ceab75
GBmWdc <63273847+GBmWdc@users.noreply.github.com>
2021-07-12 23:44:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 TOKEN = ""
 PROXIES = {
-    "http": "http://127.0.0.1:10809",
-    "https": "http://127.0.0.1:10809"
+    "http": "",
+    "https": ""
 }
 
 
